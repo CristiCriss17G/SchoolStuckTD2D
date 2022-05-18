482a4b7581c66f83324a4bd3a9afe29e6b1244e5
Cristian Iordachescu <iordachescu1996@outlook.com>
2022-05-18 21:30:08 +0300

    Initial commit

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 3f;
+    
+    private Transform endPoint;
+    
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        endPoint = GameObject.FindGameObjectWithTag("EndPoint").transform;
+        target = Waypoints.points[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.1f)
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint()
+    {
+        if (target == endPoint)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        if (wavepointIndex >= Waypoints.points.Length - 1)
+        {
+            target = endPoint;
+            return;
+        }
+        wavepointIndex++;
+        target = Waypoints.points[wavepointIndex];
+    }
+}
